34ff818ef2daf284c508daed8ad46add7e202e74
Jaroslaw Biedron <Jaroslaw_Biedron@epam.com>
2022-11-25 20:59:26 -0500

    Identity4 - initial commit

---
+++ all_solution/SRC/Identity/Identity.API/Extensions/ServiceColectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.API.Extensions
+{
+    public static class ServiceColectionExtensions
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        { 
+            // TODO !
+        }
+    }
+}
