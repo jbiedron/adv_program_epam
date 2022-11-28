b97fc69f8afa9d3643692da070e813b3870fea81
Jaroslaw Biedron <Jaroslaw_Biedron@epam.com>
2022-11-27 21:22:07 -0500

    basic token generation + in memory db

--- all_solution/SRC/Identity/Identity.API/Program.cs
+++ all_solution/SRC/Identity/Identity.API/Program.cs
@@ -1,6 +1,20 @@
+using Identity.API;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+    .AddTestUsers(InMemoryConfig.GetUsers())
+    .AddInMemoryClients(InMemoryConfig.GetClients())
+    .AddDeveloperSigningCredential();                               
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+app.UseIdentityServer();
 
 app.Run();
